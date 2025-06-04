0a934983c2f45070624bc23c3ef2f7f1085dd2ad
hiveck <144206642+hiveck@users.noreply.github.com>
2025-06-04 12:20:36 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(function () {
             // 表示＆拡大
             $(".circle").css({
                 "opacity": "1",
-                "transform": "scale(" + (1 + (scroll - appearScroll) / 50) + ")"
+                "transform": "scale(" + (1 + (scroll - appearScroll) / 30) + ")"
             });
         } else {
             // 非表示状態に戻す
@@ -32,11 +32,29 @@ $(function () {
             var targetTop = target.offset().top;
 
             // 画像が画面内に入ったら実行
-            if (scroll > targetTop - windowHeight + 100) {
+            if (scroll > targetTop - windowHeight ) {
                 target.addClass("visible");
             }
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.fade');
+    
+    const onScroll = () => {
+        elements.forEach(element => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight) {
+                element.classList.add('visible');
+            }
+        });
+    };
+    window.addEventListener('scroll', onScroll);
+    onScroll(); // 初期表示時にもチェックする
+});
+
+
 
 
